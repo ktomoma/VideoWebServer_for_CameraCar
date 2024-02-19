7620b17d634e26b9e0658b0e3443d3f7a2d3d821
ktomoma <131932595+ktomoma@users.noreply.github.com>
2024-02-19 12:05:47 -0600

    Update VideoWebServer_for_CameraCar.ino

--- VideoWebServer_for_CameraCar.ino
+++ VideoWebServer_for_CameraCar.ino
@@ -130,17 +130,15 @@ extern int VideoQuality;
 int HeadLightPin = D2; // Changed for Video Cam Car 3 in 008;
 
 // Configures static IP address
-/**
 // Set your Static IP address
-IPAddress local_IP(192, 168, 1, 201);
-IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 255, 0);
-IPAddress primaryDNS(8, 8, 8, 8);   //optional
-IPAddress secondaryDNS(192, 168, 1, 1); //optional
-if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
-   Serial.println("STA Failed to configure");
-}
-**/
+// IPAddress local_IP(192, 168, 1, 201);
+// IPAddress gateway(192, 168, 1, 1);
+// IPAddress subnet(255, 255, 255, 0);
+// IPAddress primaryDNS(8, 8, 8, 8);   //optional
+// IPAddress secondaryDNS(192, 168, 1, 1); //optional
+// if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
+//   Serial.println("STA Failed to configure");
+//}
 
 void setup() {
   pinMode(HeadLightPin, OUTPUT); 
